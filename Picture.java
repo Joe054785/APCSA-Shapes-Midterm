4b41f60e616b9369f683ececb774b9fe336c2288
Joseph Hall <jhall0188@myduneland.org>
2021-01-04 09:20:53 -0600

    Building the Picture

--- Picture.java
+++ Picture.java
@@ -9,6 +9,7 @@
 import java.io.File;
 import java.io.IOException;
 
+
 public class Picture {
     public static void main(String[] args) {
         // Get a reference to the canvas for this drawing
@@ -19,9 +20,41 @@ public class Picture {
         pic.setBackgroundColor("white");
 
         // Draw my picture
-        Circle c = new Circle();
-        c.makeVisible();
-
+        
+        //Central Circle
+        Circle central = new Circle();
+        central.makeVisible();
+        central.changeColor("black");
+        central.changeSize(120);
+        central.moveHorizontal(100);
+        central.moveVertical(150);
+        
+        //Arcing "rays"
+        Arc a = new Arc(355, 190, 120, 20, 110, "black", true);
+        Arc b = new Arc(375, 150, 120, 20, 110, "black", true);
+        Arc c = new Arc(395, 110, 120, 20, 110, "black", true);
+        Arc d = new Arc(415, 70, 120, 20, 110, "black", true);
+        Arc f = new Arc(435, 30, 120, 20, 110, "black", true);
+        Arc g = new Arc(455, -10, 120, 20, 110, "black", true);
+        
+        //"Expanding" Rectangles
+        Rect i = new Rect();
+        i.makeVisible();
+        i.moveHorizontal(-325);
+        i.moveVertical(165);
+        i.changeSize(30, 350);
+        Rect j = new Rect();
+        j.makeVisible();
+        j.moveHorizontal(135);
+        j.moveVertical(165);
+        j.changeSize(30, 360);
+        Rect k = new Rect();
+        k.makeVisible();
+        k.moveHorizontal(65);
+        k.moveVertical(235);
+        k.changeSize(300, 30);
+        
+        
         // Get the filename to save to from the command line arguments, defaulting to
         // MyPicture.png if no argument is given
         String filename;
